0cb004f7d5ea89859d0abef842036d3823ca4629
jacobdalrymple <jdalrymple1@sheffield.ac.uk>
2018-11-14 13:43:59 +0000

    added getStudentNames to student table class

--- src/sql/tables/Students.java
+++ src/sql/tables/Students.java
@@ -1,5 +1,7 @@
 package src.sql.tables;
 
+import java.util.ArrayList;;
+
 public class Students extends Table {
 
     public Students() {
@@ -8,5 +10,14 @@ public class Students extends Table {
     public void addRow(String title, String forename, String surname, String username, String email, String regNum, String levelOfStudy, String grade, String degree, String tutor) {
         super.addRow(new Object[]  {regNum, title, forename, surname, username, email, 
                                     levelOfStudy, grade, degree, tutor});
+    }
+    public ArrayList<String> getStudentNames() {
+        ArrayList<Object[]> table = getTableList();
+        int rowNum = table.size();
+        ArrayList<String> studentNames = new ArrayList<String>();
+        for (int c = 0; c < rowNum; c++) {
+            studentNames.add(((String) table.get(c)[1]) + " " +  ((String) table.get(c)[2]));
+        }
+        return studentNames;
     }
 }
