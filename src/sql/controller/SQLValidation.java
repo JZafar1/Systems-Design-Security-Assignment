d2edb0445001d4e538037cd4d531e71e02a8c001
Mieszko Szkurlat <msszkurlat1@sheffield.ac.uk>
2018-11-21 20:31:43 +0000

    Fixed login validation

--- src/sql/controller/SQLValidation.java
+++ src/sql/controller/SQLValidation.java
@@ -25,7 +25,7 @@ public class SQLValidation {
         
         int i = (int)c;
         
-        if((i>64 && i<91) || (i>96 && i<123) || (i>47 && i<58) || i == 32) return c;
+        if((i>63 && i<91) || (i>96 && i<123) || (i>47 && i<58) || i == 32 || i == 46) return c;
         else{
             System.out.println("Illegal characters used, only english alphabet letters and numbers allowed. Special characters not allowed.");
             return ' ';
